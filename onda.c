7a4a430ab229960debee970a5c27898f92e110fe
uno <uno@ubuntu.ubuntu-domain>
2014-04-24 10:38:36 -0500

    Funcion de onda

--- onda.c
+++ onda.c
@@ -30,6 +30,41 @@ float * get_memory(int n_points)
 
 int main()
 {
+  //Declaraciòn de constantes
+  int n_points=1000;
+  float xmin=0.0;
+  float xmax=1.0;
+  float deltax=(xmax-xmin)/n_points;
+  float deltat=0.005;
+  
+  //Declaraciòn de arreglos
+  float *x;
+  float *u_initial;
+  float *u_present;
+  float *u_past;
+  float *u_future;
+
+  //Condiciones iniciales y de frontera
+  x=get_memory(n_points);
+  u_initial=get_memory(n_points);
+  u_past=get_memory(n_points);
+  u_present=get_memory(n_points);
+  u_future=get_memory(n_points);
+
+  x[0]=0.0;
+
+  u_initial[0]=0.0;
+  u_initial[n_points-1]=0.0;
+
+  u_past[0]=0.0;
+  u_past[n_points-1]=0.0;
+
+  u_present[0]=0.0;
+  u_present[n_points-1]=0.0;
+
+  u_future[0]=0.0;
+  u_future[n_points-1]=0.0;
+
 
   return 0;
 }
